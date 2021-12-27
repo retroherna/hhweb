d9ced3d95135e6e6db6ddf1ccbd3bff6d259dcde
Sanqui <me@sanqui.net>
2021-12-27 16:48:34 +0100

    Create index page with projects display

---
+++ src/lib/Projects.svelte
@@ -0,0 +1,157 @@
+<div class="projects-container">
+    <div class="projects">
+        <a class="proj-retroherna" href="https://retroherna.org/">
+            <div>
+                <img src="/img/logo-retroherna.png" alt="RetroHerna">
+                <span>RetroHerna</span>
+            </div>
+        </a>
+        <a class="proj-herniarchiv" href="https://herniarchiv.cz/">
+            <div>
+                <img src="/img/logo-herniarchiv.svg" alt="Herní archiv">
+            </div>
+        </a>
+    </div>
+</div>
+
+<style>
+    .projects-container {
+        overflow-x: hidden;
+        box-shadow: 0 0 10px rgba(0,0,0,0.75);
+    }
+    .projects {
+        display: flex;
+        justify-content: space-around;
+        overflow-x: hidden;
+        width: 125%;
+        margin-left: -12.5%;
+    }
+
+    .projects > a {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
+        color: white;
+        text-decoration: none;
+        position: relative;
+        height: 200px;
+        padding-top: 32px;
+        padding-bottom: 16px;
+        z-index: 2;
+        transition: all 0.3s ease-in-out;
+    }
+    
+    .projects > a::before {
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
+        bottom: 0;
+        transform: skewX(-11deg);
+        content: '';
+        z-index: 0;
+    }
+
+    
+    .proj-retroherna::before {
+        background-color: #1b2441;
+    }
+    .proj-herniarchiv::before {
+        background-color: #f3f2e7;
+    }
+
+    .projects > a > div {
+        display: flex;
+        flex-direction: column;
+        z-index: 2;
+        text-align: center;
+        align-items: center;
+        transition: all 0.3s ease-in-out;
+    }
+
+    .proj-retroherna > div {
+        margin-left: 40%;
+        font-size: larger;
+    }
+    .proj-retroherna img {
+        width: 175px;
+        margin-bottom: 4px;
+    }
+
+    .proj-herniarchiv > div {
+        margin-right: 20%;
+        color: black;
+    }
+    .proj-herniarchiv img {
+        width: 400px;
+    }
+
+    @media screen and (max-width: 880px) {
+        .proj-herniarchiv img {
+            width: 300px;
+        }
+    }
+
+    @media screen and (min-width: 700px) {
+        .projects > a:hover {
+            width: 110%;
+        }
+
+        .projects > a:hover > div {
+            margin-top: -12px;
+        }
+    }
+
+    @media screen and (max-width: 700px) {
+        .projects-container {
+            overflow: hidden;
+            height: 400px;
+        }
+
+        .projects {
+            flex-direction: column;
+            width: 100%;
+            margin: 0;
+            margin-top: -125px;
+            overflow: hidden;
+        }
+    
+        .projects > a {
+            height: 300px;
+        }
+        .projects > a::before {
+            transform: skewY(-12deg);
+        }
+
+        .proj-retroherna img {
+            width: 125px;
+        }
+        .proj-retroherna > div {
+            margin-left: 0;
+            margin-right: 25%;
+            margin-top: 128px;
+        }
+        .proj-herniarchiv img {
+            width: 300px;
+        }
+        .proj-herniarchiv > div {
+            margin-right: 0;
+            margin-left: 25%;
+            margin-bottom: 160px;
+        }
+
+        .projects > a:hover {
+        }
+
+        .projects > a:hover > div {
+            transform: scale(1.1);
+        }
+    }
+
+    @media screen and (max-width: 400px) {
+        .proj-herniarchiv img {
+            width: 200px;
+        }
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Projects from '$lib/Projects.svelte';
+</script>
+
+<div class="container">
+    <h2>Projekty Herní historie, z.s.</h2>
+</div>
+
+<Projects />
+
+<div class="container">
+    <h2>Impressum</h2>
+    <strong>Herní historie, z.s.</strong><br>
+    <strong>IČO</strong>: 05823986<br>
+    <strong>Transparentní účet</strong>: 8686868686/0600<br>
+    <strong>Předseda výboru</strong>: Vojtěch Straka<br>
+    <strong>Email</strong>: info@hernihistorie.cz<br>
+    <strong>Telefon</strong>: 778726126
+
+    <h2>Účel spolku</h2>
+    <ul>
+        <li>Rozšiřování obecného povědomí o šíři a rozmanitosti herní historie se zaměřením na videohry. Vzdělávání zaujatých subjektů a popularizace mezi mládeží.
+        <li>Archivace fyzických objektů spojených s herní historií (datové nosiče, publikace, apod.), jejich katalogizace a ustanovení veřejně přístupného archivu.
+        <li>Použití možností digitální archivace za účelem co nejlepší dlouhodobé a organizované konzervace informací o herní historii. Používání a popularizace již zavedených metod a průzkum dalších možností ve spolupráci s tuzemskými i mezinárodními skupinami příbuzného zaměření.
+        <li>Zachování a popis herní kultury (na celosvětové, státní i regionální úrovni), vzpomínek a příběhů význačných osobností herní scény a dokumentace herních komunit, herního byznysu a vzájemného vlivu mezi dobovým prostředím a herní kulturou.
+        <li>Podpora české herní scény, spolupráce s existujícími platformami pro komunikaci a jejich vytváření. Spolupráce se světovou herní komunitou, reprezentace Česka a Slovenska v mezinárodních projektech.
+    </ul>
+</div>
+
+<style>
+    .container {
+        max-width: 800px;
+        margin: auto;
+    }
+</style>
